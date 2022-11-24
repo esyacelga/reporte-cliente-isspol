de89e0aefd8f3cb4f4f8d72c19a561124bdbc4b5
SY <esyacelga@hotmail.com>
2022-11-24 13:35:33 -0500

    Integracion modulo reporte

---
+++ rest-reporte-generador/src/main/java/ec/org/isspol/mic/rest/reporte/restreportegenerador/RestReporteGeneradorApplication.java
@@ -0,0 +1,13 @@
+package ec.org.isspol.mic.rest.reporte.restreportegenerador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestReporteGeneradorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestReporteGeneradorApplication.class, args);
+    }
+
+}
---
+++ rest-reporte-generador/src/test/java/ec/org/isspol/mic/rest/reporte/restreportegenerador/RestReporteGeneradorApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.org.isspol.mic.rest.reporte.restreportegenerador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestReporteGeneradorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
