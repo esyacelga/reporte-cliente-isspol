677f8e9263aec11f0ab4dc3c7a89831a09c6591d
SY <esyacelga@hotmail.com>
2022-11-24 11:16:44 -0500

    Creacion mòdulo

---
+++ micro-servicio-generacion-reporte/src/main/java/com/org/isspol/mcreportegeneracion/MicroServicioReporteGeneracionApplication.java
@@ -0,0 +1,13 @@
+package com.org.isspol.mcreportegeneracion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicioReporteGeneracionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServicioReporteGeneracionApplication.class, args);
+    }
+
+}
---
+++ micro-servicio-generacion-reporte/src/test/java/com/org/isspol/mcreportegeneracion/MicroServicioReporteGeneracionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.isspol.mcreportegeneracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicioReporteGeneracionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
